af81d03f9f155bf3eebad3b01683bbc7ee140a74
ZhuoranCool <161045321+ZhuoranCool@users.noreply.github.com>
2024-09-10 10:22:31 -0400

    all pages except gallery

---
+++ main.js
@@ -0,0 +1,15 @@
+// main.js 文件内容
+const app = Vue.createApp({
+    data() {
+      return {
+        message: 'Want to Catch our Activities?',
+        logo_image: 'assets/pictures/Logo.png',
+        pitt_image: 'assets/pictures/Pitt_Image.jpg',
+        instagram_icon: 'assets/pictures/Instagram_Icon.png',
+        email_icon: 'assets/pictures/Email_Icon.png',
+        discord_icon: 'assets/pictures/Discord_Icon.png'
+      }
+    }
+  });
+  
+  app.mount('#app');
